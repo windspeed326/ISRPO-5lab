1c713978721eca72ec4dca4cd7699915e87b96f8
windspeed326 <nikitagontar9@gmail.com>
2025-12-18 23:39:18 +0300

    Add files via upload

---
+++ Gontar-ISRPO5lab/WindowsFormsApplication8/Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsFormsApplication8
+{
+    internal class Class1
+    {
+        public static double Add(double a, double b) => a + b;
+
+        public static double Subtract(double a, double b) => a - b;
+
+        public static double Multiply(double a, double b) => a * b;
+
+        public static double Divide(double a, double b)
+        {
+            if (b == 0)
+                throw new DivideByZeroException("Деление на ноль невозможно");
+            return a / b;
+        }
+
+        public static double Power(double a, double exponent) => Math.Pow(a, exponent);
+
+        public static double SquareRoot(double a)
+        {
+            if (a < 0)
+                throw new ArgumentException("Корень из отрицательного числа невозможен");
+            return Math.Sqrt(a);
+        }
+
+        public static double Square(double a) => a * a;
+
+        public static double Percent(double value, double percent) => value * percent / 100;
+
+        public static double Reciprocal(double a)
+        {
+            if (a == 0)
+                throw new DivideByZeroException("Обратное число для нуля не существует");
+            return 1 / a;
+        }
+
+        public static double Negate(double a) => -a;
+    }
+}
---
+++ Gontar-ISRPO5lab/WindowsFormsApplication8/Class2.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsFormsApplication8
+{
+    internal class Class2
+    {
+        public static double Sin(double angle, bool isRadians = false)
+        {
+            double angleInRadians = isRadians ? angle : DegreesToRadians(angle);
+            return Math.Round(Math.Sin(angleInRadians), 10);
+        }
+
+        public static double Cos(double angle, bool isRadians = false)
+        {
+            double angleInRadians = isRadians ? angle : DegreesToRadians(angle);
+            return Math.Round(Math.Cos(angleInRadians), 10);
+        }
+
+        public static double Tan(double angle, bool isRadians = false)
+        {
+            double angleInRadians = isRadians ? angle : DegreesToRadians(angle);
+            return Math.Round(Math.Tan(angleInRadians), 10);
+        }
+
+        public static double Cot(double angle, bool isRadians = false)
+        {
+            double tanValue = Tan(angle, isRadians);
+            if (tanValue == 0)
+                throw new DivideByZeroException("Котангенс не существует для данного угла");
+            return 1 / tanValue;
+        }
+
+        public static double Asin(double value)
+        {
+            if (value < -1 || value > 1)
+                throw new ArgumentException("Аргумент должен быть в диапазоне [-1, 1]");
+            return Math.Asin(value);
+        }
+
+        public static double Acos(double value)
+        {
+            if (value < -1 || value > 1)
+                throw new ArgumentException("Аргумент должен быть в диапазоне [-1, 1]");
+            return Math.Acos(value);
+        }
+
+        public static double Atan(double value) => Math.Atan(value);
+
+        public static double Sinh(double angle) => Math.Sinh(angle);
+
+        public static double Cosh(double angle) => Math.Cosh(angle);
+
+        public static double Tanh(double angle) => Math.Tanh(angle);
+
+        public static double DegreesToRadians(double degrees) => degrees * Math.PI / 180;
+
+        public static double RadiansToDegrees(double radians) => radians * 180 / Math.PI;
+    }
+}
